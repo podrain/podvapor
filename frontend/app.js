b03e0615c4859df2b59f0d1168e57be9c559d545
Joe Sweeney <joe.sweeney224@gmail.com>
2022-02-14 23:58:29 -0700

    inertia progress bar

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,12 @@
 import { createApp, h } from 'vue'
 import { App, plugin } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
 import './app.css'
 import Button from './shared/button.vue'
 import Input from './shared/input.vue'
 import TextArea from './shared/textarea.vue'
+
+InertiaProgress.init()
 
 const el = document.getElementById('app')
 let Pages = import.meta.glob('./pages/**/*.vue')
