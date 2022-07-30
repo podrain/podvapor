ce90a1080f1d830d426abb51b6cc5067cfeb5ae5
Joe Sweeney <joe.sweeney224@gmail.com>
2022-07-29 23:29:55 -0600

    secure session cookie

--- backend/session.ts
+++ backend/session.ts
@@ -8,6 +8,7 @@ await store.initSessionsTable()
 export default Session.initMiddleware(store, {
   expireAfterSeconds: 900,
   cookieSetOptions: {
-    sameSite: 'lax'
+    sameSite: 'lax',
+    secure: new URL(Deno.env.get('DOMAIN') ?? 'http://localhost').protocol === 'https:' ? true : false
   }
 })
